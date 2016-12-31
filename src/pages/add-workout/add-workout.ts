b0d9a53d3eef3985ef57ca4a2126281cfea23053
Luis Manuel Cruza <luismanuel.cruza@gmail.com>
2016-12-31 15:28:11 +0100

    first commit

---
+++ src/pages/add-workout/add-workout.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+    selector: 'add-workout',
+    templateUrl: 'add-workout.html'
+})
+export class AddWorkoutPage implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
---
+++ src/pages/workouts/workouts.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+    selector: 'workouts',
+    templateUrl: 'workout.html'
+})
+export class WorkoutsPage implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
